29d5c0ded03cdc4ed3912d0809535ba439ccbc36
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2014-09-25 17:55:16 -1000

    Tailor linting is applied.

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,3 @@ license          'GNU Public License 3.0'
 description      'Installs/Configures chef-nodeAttributes'
 long_description 'Installs/Configures chef-nodeAttributes'
 version          '0.1.0'
-
